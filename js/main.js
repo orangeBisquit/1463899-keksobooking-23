6218f5da60fbba2df0236524fab71270c75bb18a
Artem <theuselessn@gmail.com>
2021-06-01 19:01:15 +0600

    removed unnesecary if

--- js/main.js
+++ js/main.js
@@ -3,16 +3,14 @@ const getRandomFloat = (min, max, float) => {
   if (
     (Number.isNaN(min, max, float) || typeof min !== 'number' || typeof max !== 'number' || typeof float !== 'number')
   ) {
-    throw new Error();
+    throw new Error('min, max или float не являются числами');
   }
   const from = Math.min(min, max);
   const to = Math.max(min, max);
   const exponentBase = Math.pow(10, float);
 
-  if (from >= 0 && to >= 0) {
-    return (Math.round((Math.random() * (to - from) + from) * exponentBase) / exponentBase);
-  }
-  return (Math.round((Math.random() * (from - to) + to) * exponentBase) / exponentBase);
+  return (Math.round((Math.random() * (to - from) + from) * exponentBase) / exponentBase);
+
 };
 
 const getRandomInteger = (min, max) => getRandomFloat(min, max, 0);
