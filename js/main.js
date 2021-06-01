7fdc63bb4cedbee1e2192b42608fe1cdf7337834
Artem <theuselessn@gmail.com>
2021-06-01 18:24:16 +0600

    duplicate code removed

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 const getRandomFloat = (min, max, float) => {
 
-  const exponentBase = Math.pow(10, float);
-
+  if (
+    (Number.isNaN(min, max, float) || typeof min !== 'number' || typeof max !== 'number' || typeof float !== 'number')
+  ) {
+    throw new Error();
+  }
   const from = Math.min(min, max);
   const to = Math.max(min, max);
+  const exponentBase = Math.pow(10, float);
 
-  const checkIsNaN = () => !Number.isNaN(from, to, float);
-
-  if (from >= 0 && to >= 0 && checkIsNaN()) {
-    return (
-      Math.round((Math.random() * (to - from) + from) * exponentBase) /
-      exponentBase
-    );
-  } else if (checkIsNaN()) {
-    return (
-      Math.round((Math.random() * (from - to) + to) * exponentBase) /
-      exponentBase
-    );
+  if (from >= 0 && to >= 0) {
+    return (Math.round((Math.random() * (to - from) + from) * exponentBase) / exponentBase);
   }
+  return (Math.round((Math.random() * (from - to) + to) * exponentBase) / exponentBase);
 };
 
 const getRandomInteger = (min, max) => getRandomFloat(min, max, 0);
 getRandomInteger(1, 4);
+
