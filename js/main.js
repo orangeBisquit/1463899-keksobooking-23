3356e6eb5775805eb2299bf97ca536b25f54b135
Artem <theuselessn@gmail.com>
2021-06-01 11:59:42 +0600

    float and integer functions fixes

--- js/main.js
+++ js/main.js
@@ -1,30 +1,11 @@
 const getRandomFloat = (min, max, float) => {
-  if (min >= 0 && max >= 0 && min < max) {
-    return Math.round((Math.random() * (max - min) + min) * Math.pow(10, float)) /
-      Math.pow(10, float);
+
+  const from = Math.min(min, max);
+  const to = Math.max(min, max);
+
+  if (from >= 0 && to >= 0) {
+    return (Math.random() * (to - from) + from).toFixed(float);
   }
 };
 
-getRandomFloat(20, 30, 3);
-getRandomFloat(40, 30, 3);
-// DELETE
-// console.log(getRandomFloat(20, 30, 3));
-// console.log(getRandomFloat(20, 30, 3));
-// console.log(getRandomFloat(20, 30, 3));
-// console.log(getRandomFloat(40, 30, 3));
-
-
-const getRandomInteger = (min, max) => {
-  if (min >= 0 && max >= 0 && min < max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-};
-
-getRandomInteger(10, 15);
-getRandomInteger(17, 15);
-// DELETE
-// console.log(getRandomInteger(10, 15));
-// console.log(getRandomInteger(10, 15));
-// console.log(getRandomInteger(10, 15));
-// console.log(getRandomInteger(17, 15));
-
+const getRandomInteger = (min, max) => getRandomFloat(min, max, 0);
