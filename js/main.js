55ca85e8eaa75f1aeb4e1863f4d81ef5b97e8900
Artem <theuselessn@gmail.com>
2021-06-12 11:54:57 +0600

    'naming fixed, variables added'

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const TEMP_MAX_ROOMS = 10;
 
 const TEMP_MAX_GUESTS = 30;
 
-const OBJECTS_NUMBER = 10;
+const ADS_NUMBER = 10;
 
 const getRandomFloat = (min, max, float) => {
   if (
@@ -28,8 +28,6 @@ const getRandomFloat = (min, max, float) => {
 const getRandomInteger = (min, max) => getRandomFloat(min, max, 0);
 getRandomInteger(1, 4);
 
-// module4 task 1
-
 // Выбрать рандомные элементы массива
 const getRandomArrayValues = (array) => {
   const shuffledArray = array.sort(() => 0.5 - Math.random());
@@ -48,11 +46,21 @@ const TempPrice = {
   priceMax: 1000,
 };
 
+const GeoLocation = {
+  lat: {
+    min: 35.65,
+    max: 35.7,
+  },
+  lng: {
+    min: 139.7,
+    max: 139.8,
+  },
+  float: 5,
+};
+
 const TYPES = ['palace', 'flat', 'house', 'bungalow'];
 
 const CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
-
-const CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
 
 const FEATURES = [
   'wifi',
@@ -69,64 +77,58 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const GeoLocation = {
-  lat: {
-    min: 35.65,
-    max: 35.7,
-  },
-  lng: {
-    min: 139.7,
-    max: 139.8,
-  },
-  float: 5,
+const getAd = () => {
+
+  const locationLat = getRandomFloat(
+    GeoLocation.lat.min,
+    GeoLocation.lat.max,
+    GeoLocation.float,
+  );
+  const locationLng = getRandomFloat(
+    GeoLocation.lng.min,
+    GeoLocation.lng.max,
+    GeoLocation.float,
+  );
+
+  const checkInAndOut =
+    CHECKIN_TIMES[getRandomInteger(0, CHECKIN_TIMES.length - 1)];
+
+  return {
+    author: {
+      avatar: `${AVATAR_DIR}${String(getRandomInteger(0, 10)).padStart(
+        2,
+        '0',
+      )}'.png'`,
+    },
+    offer: {
+      title: 'Объявление',
+      address: `${locationLat}, ${locationLng}`,
+      price: getRandomInteger(TempPrice.priceMin, TempPrice.priceMax),
+      type: TYPES[getRandomInteger(0, TYPES.length - 1)],
+      rooms: getRandomInteger(1, TEMP_MAX_ROOMS),
+      guests: getRandomInteger(1, TEMP_MAX_GUESTS),
+      checkin: checkInAndOut,
+      checkout: checkInAndOut,
+      features: getRandomArrayValues(FEATURES),
+      description: 'Описание',
+      photos: getRandomArrayValues(PHOTOS),
+    },
+    location: {
+      lat: locationLat,
+      lng: locationLng,
+    },
+  };};
+
+const getAds = () => {
+  const randomAds = [];
+
+  for (let i = 0; i < ADS_NUMBER; i++) {
+    randomAds.push(getAd());
+  }
+
+  return randomAds;
 };
 
-const createRandomObj = () => ({
-  author: {
-    avatar: `${AVATAR_DIR}${String(getRandomInteger(0, 10)).padStart(
-      2,
-      '0',
-    )}'.png'`,
-  },
-  offer: {
-    title: 'Объявление',
-    address: `${getRandomFloat(
-      GeoLocation.lat.min,
-      GeoLocation.lat.max,
-      GeoLocation.float,
-    )}, ${getRandomFloat(
-      GeoLocation.lng.min,
-      GeoLocation.lng.max,
-      GeoLocation.float,
-    )}`,
-    price: getRandomInteger(TempPrice.priceMin, TempPrice.priceMax),
-    type: TYPES[getRandomInteger(0, TYPES.length - 1)],
-    rooms: getRandomInteger(1, TEMP_MAX_ROOMS),
-    guests: getRandomInteger(1, TEMP_MAX_GUESTS),
-    checkin: CHECKIN_TIMES[getRandomInteger(0, CHECKIN_TIMES.length - 1)],
-    checkout: CHECKOUT_TIMES[getRandomInteger(0, CHECKOUT_TIMES.length - 1)],
-    features: getRandomArrayValues(FEATURES),
-    description: 'Описание',
-    photos: getRandomArrayValues(PHOTOS),
-  },
-  location: {
-    lat: getRandomFloat(
-      GeoLocation.lat.min,
-      GeoLocation.lat.max,
-      GeoLocation.float,
-    ),
-    lng: getRandomFloat(
-      GeoLocation.lng.min,
-      GeoLocation.lng.max,
-      GeoLocation.float,
-    ),
-  },
-});
+const myAds = getAds();
 
-const randomObjects = [];
-
-for (let i = 0; i < OBJECTS_NUMBER; i++) {
-  randomObjects.push(createRandomObj());
-}
-
-randomObjects;
+myAds;
