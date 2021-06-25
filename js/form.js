fa79dbbce2ac0559872488ce69e87fddd1a6bd1b
Artem <theuselessn@gmail.com>
2021-06-25 09:23:44 +0600

    type and time validation added

--- js/form.js
+++ js/form.js
@@ -3,13 +3,24 @@ const AD_HEADING = AD_FORM.querySelector('#title');
 const AD_PRICE = AD_FORM.querySelector('#price');
 const AD_ROOM = AD_FORM.querySelector('#room_number');
 const AD_CAPACITY = AD_FORM.querySelector('#capacity');
+const AD_TYPE = AD_FORM.querySelector('#type');
 const AD_CAPACITY_OPTIONS = AD_CAPACITY.querySelectorAll('option');
+const AD_TIMEIN = AD_FORM.querySelector('#timein');
+const AD_TIMEOUT = AD_FORM.querySelector('#timeout');
 
 const ROOM_OPTIONS = {
   1: [1],
   2: [1, 2],
   3: [1, 2, 3],
   100: [0],
+};
+
+const ROOM_PRICE = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
 };
 
 const headingInputHandler = () => {
@@ -31,10 +42,13 @@ const headingInputHandler = () => {
 };
 
 const priceInputHandler = () => {
-  const value = AD_PRICE.value.length;
+  const value = AD_PRICE.value;
   const maxValue = AD_PRICE.getAttribute('max');
+  const minValue = AD_PRICE.getAttribute('min');
 
-  if (value > maxValue) {
+  if (value < Number(minValue)) {
+    AD_PRICE.setCustomValidity(`Минимальная цена ${minValue} рублей`);
+  } else if (value > Number(maxValue)) {
     AD_PRICE.setCustomValidity(`Максимальная цена ${maxValue} рублей`);
   } else {
     AD_PRICE.setCustomValidity('');
@@ -72,20 +86,34 @@ const roomChangeHandler = (evt) => {
   setCapacity(evt.target.value);
 };
 
+const typeChangeTypeHandler = (evt) => {
+  AD_PRICE.setAttribute('min', ROOM_PRICE[evt.target.value]);
+  AD_PRICE.setAttribute('placeholder', ROOM_PRICE[evt.target.value]);
+};
+
+const timeChangeHandler = (evt) => {
+  AD_TIMEIN.value = evt.target.value;
+  AD_TIMEOUT.value = evt.target.value;
+};
+
 const enableFormValidation = () => {
   disableCapacity(true);
 
   AD_HEADING.addEventListener('input', headingInputHandler);
   AD_PRICE.addEventListener('input', priceInputHandler);
   AD_ROOM.addEventListener('change', roomChangeHandler);
+  AD_TYPE.addEventListener('change', typeChangeTypeHandler);
+  AD_TIMEIN.addEventListener('change', timeChangeHandler);
+  AD_TIMEOUT.addEventListener('change', timeChangeHandler);
 };
 
 const disableFormValidation = () => {
   AD_HEADING.removeEventListener('input', headingInputHandler);
   AD_PRICE.removeEventListener('input', priceInputHandler);
   AD_ROOM.removeEventListener('change', roomChangeHandler);
+  AD_TYPE.removeEventListener('change', typeChangeTypeHandler);
+  AD_TIMEIN.removeEventListener('change', timeChangeHandler);
+  AD_TIMEOUT.removeEventListener('change', timeChangeHandler);
 };
 
 export { enableFormValidation, disableFormValidation };
-
-
