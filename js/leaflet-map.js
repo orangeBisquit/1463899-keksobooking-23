65c8ea155433cbfc55314d3fa1b775d0a6fe977c
Artem <theuselessn@gmail.com>
2021-06-25 19:28:53 +0600

    img problems

---
+++ js/leaflet-map.js
@@ -0,0 +1,34 @@
+const map = L.map("map-canvas")
+  .on("load", () => {})
+  .setView(
+    {
+      lat: 35.74205383068037,
+      lng: 139.83755498193207,
+    },
+    10
+  );
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '../leaflet/images/marker-icon.png',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+})
+
+const marker = L.marker(
+  {
+    lat: 35.74205383068037,
+    lng: 139.83755498193207,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  }
+);
+
+marker.addTo(map);
+
--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-import {getAds} from './data.js';
-import {createCard, renderCard} from './render-card.js';
+// import {getAds} from './data.js';
+// import {createCard, renderCard} from './render-card.js';
 import {disablePage, enablePage} from './page-state.js';
 import './form.js';
+import './leaflet-map.js';
 
-const myAds = getAds();
+// const myAds = getAds();
 
-renderCard(createCard(myAds[0]));
+// renderCard(createCard(myAds[0]));
 
 window.addEventListener('load', disablePage);
 window.addEventListener('load', enablePage);
